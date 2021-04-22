518c33e38b52e59de488f794a6df94072b857924
ehubert17 <hubert.52@buckeyemail.osu.edu>
2021-04-22 11:43:11 -0400

    changed user.rb to have names be case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    before_save { self.name = name.downcase }
-    validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+    validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: true }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 } 
 end
