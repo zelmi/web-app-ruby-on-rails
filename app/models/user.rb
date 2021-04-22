9965a5d8262f99917027327da8ebc17ebf035b5a
zehur elmi <zehurelmi@example.com>
2021-04-22 10:15:35 -0400

    make sure new passwords are at least 6 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     before_save { self.name = name.downcase }
-    has_secure_password
     validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
-    validates :password_digest, presence: true
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 } 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,3 +31,4 @@ class UserTest < ActiveSupport::TestCase
   end
    
 end
+end
